699664751bd9d320cc6ccc6030397337e223d065
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2024-12-03 10:15:06 +0100

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,20 +1,17 @@
 import re
 import math
-with open('input3.txt') as f: lines = f.readlines()
+with open('input3.txt') as f: matches = [re.findall(r'do\(\)|don\'t\(\)|mul\(\d{1,3},\d{1,3}\)', line) for line in f.readlines()]
 def extract_and_multiply(mul): return math.prod(list(map(int, re.findall(r'\d+', mul))))
 
-# part 1
-matches = [re.findall(r'mul\(\d+,\d+\)', line) for line in lines]
-products = [extract_and_multiply(m) for sublist in matches for m in sublist]
-print(sum(products))
-
-# part 2
-matches = [re.findall(r'do\(\)|don\'t\(\)|mul\(\d+,\d+\)', line) for line in lines]
-do, s = True, 0
+do, p1, p2 = True, 0, 0
 for sublist in matches:
     for m in sublist:
         match m:
             case 'do()': do = True
             case 'don\'t()': do = False
-            case _ if do: s += extract_and_multiply(m)
-print(s)
+            case _:
+                toAdd = extract_and_multiply(m)
+                p1 += toAdd
+                if do: p2 += toAdd
+print(p1)
+print(p2)
