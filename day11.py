2f689484e8af96222faefe239606ffc83a2c8256
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2024-12-11 18:50:20 +0100

    Update day11.py

--- day11.py
+++ day11.py
@@ -5,12 +5,10 @@ numbers = list(map(int, open('input11.txt').read().split()))
 def evolve(number, times):
     if times == 0: return 1
     if number == 0: return evolve(1, times-1)
-    if (l := len(str(number))) % 2 == 0:
-        return sum(evolve(n, times-1) for n in divmod(number, 10**(l//2)))
-    return evolve(number*2024, times-1)
+    if (l := len(str(number))) % 2 == 1: return evolve(number*2024, times-1)
+    return sum(evolve(n, times-1) for n in divmod(number, 10**(l//2)))
 
 # Part 1
 print(sum(evolve(n, 25) for n in numbers))
-
 # Part 2
 print(sum(evolve(n, 75) for n in numbers))
